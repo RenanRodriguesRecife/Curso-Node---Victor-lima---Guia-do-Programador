4a814e6fab665214bc5fb97ba26e64fab61ae175
Renan Rodrigues <renan_ime_2012@hotmail.com>
2021-10-09 14:37:29 -0300

    adição de tabelas

--- 03-express/db.js
+++ 03-express/db.js
@@ -4,6 +4,40 @@ const sequelize = new Sequelize('sistemadecadastro','root','123456',{
     dialect: 'mysql'
 })
 
+const Postagem = sequelize.define('postagens',{
+    título:{
+        type: Sequelize.STRING 
+    },
+    conteudo:{
+        type: Sequelize.TEXT
+    }
+})
+
+//Postagem.sync({force: true})
+Postagem.create({titulo: "Um título qualquer", conteudo: "hfdklahfdajklaldfhl"})
+
+const Usuario = sequelize.define('usuarios',{
+    nome: {
+        type: Sequelize.STRING
+    },
+    sobrenome: {
+        type: Sequelize.STRING
+    },
+    idade: {
+        type: Sequelize.INTEGER
+    },
+    email: {
+        type: Sequelize.STRING
+    }
+})
+
+//Usuario.sync({force: true})
+Usuario.create({
+    nome: "renan",
+    sobrenome: "rwqkl",
+    idade: 34,
+    email: "fdjkaslfjkçlasfjç"
+})
 
 sequelize.authenticate().then(function(){
     console.log("Conectado com sucesso!")
